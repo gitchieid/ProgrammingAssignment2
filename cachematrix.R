ba586e928fe369814aec7e254fa7f0e387e986c1
gitchieid <e.j@unix.net>
2014-09-21 19:43:57 -0700

    Updated comments
    Added some more detail to the comments to explain how to use the
    functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,17 @@
 ## This pair of functions works together to cache the value of a matrix 
-## inversion after it has been calculated once, avoiding costly computation
+## inversion after it has been calculated once, avoiding costly re-computation
 
-## This first function creates a list of four functions that set and get
-## both the matrix itself and its inverse.  The four functions are set,
-## get, setinverse and getinverse.
+## This first function creates a list.  The list contains four functions 
+## that can be used to get and set the matrix and its inverse.  The four 
+## functions are:
+##       1) set, 
+##       2) get, 
+##       3) setinverse, and 
+##       4  getinverse.
+##
+## How to use this function:
+## 
+## y <- makeCacheMatrix()
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -14,7 +22,7 @@ makeCacheMatrix <- function(x = matrix()) {
 		i <<- NULL
 
 	}
-	get <- function() x
+	get <- function() xx
 	setinverse <- function(inverse) i <<- inverse
 	getinverse <- function() i
 	list(set = set, get = get,
@@ -27,11 +35,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Before it does, it checks to see if the inverse has already been 
 ## calculated and, if it has, simply returns the cached result, rather than
 ## recalculating it.  This avoids repeating a computationally intensive task.
-
+##
+## Example:
+##
+## x <- makeCacheMatrix(rnorm(16),4,4) (NB: the matrix must be square)
+## cacheSolve(x)
+##
+## Notice that if you then run cacheSolve again, you get "Getting cached..."
 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## Return a matrix that is the inverse of 'x'
 
 	i <- x$getinverse()
 	if(!is.null(i)) {
